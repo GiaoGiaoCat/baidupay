dea5d77d80769c348dd9edf3994298fcc40922f1
Victor Wang <wjp2013@gmail.com>
2018-10-09 19:52:11 +0800

    实现签名功能

--- lib/baidupay.rb
+++ lib/baidupay.rb
@@ -1,5 +1,26 @@
-require "baidupay/version"
+require 'net/http'
+require 'cgi'
+require 'baidupay/version'
+require 'baidupay/utils'
+require 'baidupay/sign/rsa'
+# require 'baidupay/service'
+# require 'baidupay/notify'
+# require 'baidupay/wap/service'
+# require 'baidupay/wap/notify'
+# require 'baidupay/wap/sign'
+# require 'baidupay/mobile/service'
+# require 'baidupay/mobile/sign'
+require 'baidupay/client'
 
 module Baidupay
-  # Your code goes here...
+  @debug_mode = true
+  @sign_type = 'MD5'
+
+  class << self
+    attr_accessor :pid, :key, :sign_type, :debug_mode
+
+    def debug_mode?
+      !!@debug_mode
+    end
+  end
 end
---
+++ lib/baidupay/client.rb
@@ -0,0 +1,38 @@
+module Baidupay
+  class Client
+    attr_accessor :deal_id, :app_key, :public_key, :private_key
+
+    def initialize(options)
+      options = ::Baidupay::Utils.stringify_keys(options)
+      @deal_id = options['deal_id']
+      @app_key = options['app_key']
+      @public_key = options['public_key']
+      @private_key = options['private_key']
+    end
+
+    # Generate sign for params.
+    #
+    # Example:
+    #
+    #   baidupay_client.sign(tp_order_id: 'NO129388445')
+    #   # => 'app_id=2016000000000000&charset=utf-8&sig....'
+    def sign(params)
+      string = params_to_string(prepare_params(params))
+      ::Baidupay::Sign::RSA.sign(private_key, string)
+    end
+
+    private
+
+    def prepare_params(params)
+      params = ::Baidupay::Utils.stringify_keys(params)
+      prepare_params = {
+        'appKey' => app_key, 'dealId' => deal_id, 'tpOrderId' => params['tp_order_id']
+      }
+      prepare_params
+    end
+
+    def params_to_string(params)
+      params.sort.map { |item| item.join('=') }.join('&')
+    end
+  end
+end
